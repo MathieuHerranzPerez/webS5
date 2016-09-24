0155be00ff5d6b4c7f504716c85849170989a4f5
Mathieu HP <mathieu.herranz-perez@etu.univ-amu.fr>
2016-09-24 19:30:21 +0200

    swap de css (! cookies)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+function setStyle(value) {
+    $('#css').attr('href', 'css/' + value);
 
+    var expiration = new Date();
+    expiration.setTime(expiration.getTime() + 3600000);
+    document.cookie = "style=" + value + "; expires=" + expiration.toGMTString();
+}
+
+function getStyleCookie() {
+    var cookieArray = document.cookie.split(';');
+    for( var i = 0; cookieArray.length > i; ++i ){
+        var cookieStr = cookieArray[i];
+        var nameplace = cookieStr.indexOf("style=");
+        if( nameplace != -1)
+            return cookieStr.substring((nameplace + 6), cookieStr.length);
+    }
+    return "";
+}
+
+function readStyleCookie() {
+    var style = getStyleCookie();
+    if( style != "" ) {
+        $('#css').attr('herf', 'css/' + style);
+    }
+}
