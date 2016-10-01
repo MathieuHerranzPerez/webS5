49ec5b36c081f62901eff9e6c3263b2c2f603d24
Mathieu HP <mathieu.herranz-perez@etu.univ-amu.fr>
2016-10-01 12:56:24 +0200

    dl cv + head

--- cv.php
+++ cv.php
@@ -12,7 +12,21 @@
         ?>
         <div class="container">
             <a href="download/CV_General_MathieuHerranzPerez.pdf">Mon Cv</a>
+            <hr/>
+            <div>
+                <h2><stong>Mathieu HERRANZ PEREZ</stong></h2>
+                <p>Bât. K, av. JP Coste, rés. Le Bel Ormeau
+                   13100 Aix-en-Provence</p>
+                <strong>06 70 88 22 85<br/>
+                    mathieu.herranzperez@gmail.com<br/>
+                    <a href="https://github.com/MathieuHerranzPerez"><span class="maj">github</span></a><br/>
+                </strong><br/>
+                20 ans<br/>
+                Nationalité française, Permis B
+
+            </div>
         </div>
+
         <?php
             displayFooter();
         ?>
